2baae0ea070937191ca1a03a36ecf473b9c71e0c
JamshadAhmad <jamshadahmad.se@gmail.com>
2020-07-15 22:35:06 +0500

    add posts pages and handler

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
+
+  # renders post listing/index page
   def index
-    #todo implement
+    @posts = Post.all.order("created_at DESC")
   end
 
+  # renders the new post page
   def new
     @post = Post.new
   end
 
+  # inserts the new post
   def create
     @post = Post.new(post_params)
 
@@ -17,8 +21,33 @@ class PostsController < ApplicationController
     end
   end
 
+  # renders show post page
   def show
     @post = Post.find(params[:id])
+  end
+
+  # renders the edit page
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # updates the actual record
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  # handles delete hit
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
