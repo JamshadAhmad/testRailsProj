35c9132d418b3b1f4fc6d81ee387ee0c64085da2
JamshadAhmad <jamshadahmad.se@gmail.com>
2020-07-15 23:36:54 +0500

    implement comments insertion/deletion

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy # Without this dependent, comment don't get delete with Post, This applies cascading I think
 end
