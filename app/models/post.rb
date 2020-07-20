5302be4f6e836ed6486d87616e431c1e97497e1f
JamshadAhmad <jamshadahmad.se@gmail.com>
2020-07-20 17:28:15 +0500

    add AR callback before save

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy # Without this dependent, comment don't get delete with Post, This applies cascading I think
+  before_save :replace_special_chars
+
+  private
+
+  def replace_special_chars
+    replacements = { ' ' => '_', '-' => '_' }
+    self.title = title.strip.split('').map { |i| replacements[i] || i }.join
+  end
 end
