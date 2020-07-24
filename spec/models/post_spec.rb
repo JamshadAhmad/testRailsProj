649f0361288c1c66cfb5360d72002b009a3aa441
JamshadAhmad <jamshadahmad.se@gmail.com>
2020-07-24 11:32:42 +0500

    add let description + add custom error message

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,7 +7,11 @@ RSpec.configure do |config|
 end
 
 RSpec.describe Post, type: :model do
-  let(:post) { Post.new }
+  let(:post) { Post.new } # lazy loaded, will only run once per example it is being used
+  # let!(:post) will actually run before each example even if it is not used there
+  # it is now behaving like `before do` block
+  let(:x) { 1 + 1 }
+  let(:y) { x + 3 } # we can use one let variable in another one as well
 
   it 'checks creation of a new post and tests before save callback to replace special characters' do
     post.title = ' abc-  '
@@ -15,6 +19,10 @@ RSpec.describe Post, type: :model do
     post.save
 
     expect(post.title).to eq('abc_')
+
+    post.title = 'something-new '
+
+    expect(post.title).to eq('something-new ')
   end
 
   it 'checks creation of a post whose title should not change' do
@@ -23,5 +31,9 @@ RSpec.describe Post, type: :model do
     post.save
 
     expect(post.title).to eq('xy_zabs')
+  end
+
+  it 'is a sample test to check behavior of let' do
+    expect(y).to eq(5), 'this is a custom error message' # putting a custom error message
   end
 end
